c9c5fbe6f45476e67c5d7c824139188f08084b9d
thonghp <thonghoangpham123@gmail.com>
2022-09-01 21:39:34 +0700

    add query code create user and role table in database

--- sql/createDatabaseScript.sql
+++ sql/createDatabaseScript.sql
@@ -4,119 +4,44 @@ CREATE
 USE
     `bookstoredb`;
 
-CREATE TABLE `users`
+CREATE TABLE `role`
 (
-    `user_id`   int(11)     NOT NULL AUTO_INCREMENT,
-    `email`     varchar(30) NOT NULL,
-    `password`  varchar(16) NOT NULL,
-    `full_name` varchar(30) NOT NULL,
-    PRIMARY KEY (`user_id`),
-    UNIQUE KEY `user_id_UNIQUE` (`user_id`)
+    `role_id` int         NOT NULL AUTO_INCREMENT,
+    `name`    varchar(40) NOT NULL,
+    PRIMARY KEY (`role_id`),
+    UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 19
-  DEFAULT CHARSET = utf8;
+  AUTO_INCREMENT = 3
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE `user`
+(
+    `user_id`   int                             NOT NULL AUTO_INCREMENT,
+    `email`     varchar(128) CHARACTER SET utf8 NOT NULL,
+    `password`  varchar(64) CHARACTER SET utf8  NOT NULL,
+    `full_name` varchar(150) CHARACTER SET utf8 NOT NULL,
+    `enabled`   bit(1)                          NOT NULL,
+    `photos`    varchar(64) DEFAULT NULL,
+    `role_id`   int                             NOT NULL,
+    PRIMARY KEY (`user_id`),
+    UNIQUE KEY `email_UNIQUE` (`email`),
+    KEY `role_fk_idx` (`role_id`),
+    CONSTRAINT `role_fk` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE `category`
 (
-    `category_id` int          NOT NULL AUTO_INCREMENT,
-    `name`        varchar(128) NOT NULL,
-    `alias`       varchar(256) NOT NULL,
-    `enabled`     bit(1)       NOT NULL,
+    `category_id` int                             NOT NULL AUTO_INCREMENT,
+    `name`        varchar(128) CHARACTER SET utf8 NOT NULL,
+    `alias`       varchar(256) CHARACTER SET utf8 NOT NULL,
+    `enabled`     bit(1)                          NOT NULL,
     PRIMARY KEY (`category_id`),
     UNIQUE KEY `category_id_UNIQUE` (`category_id`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 11
-  DEFAULT CHARSET = utf8mb3;
-
-CREATE TABLE `book`
-(
-    `book_id`          int(11)      NOT NULL AUTO_INCREMENT,
-    `title`            varchar(128) NOT NULL,
-    `author`           varchar(64)  NOT NULL,
-    `description`      mediumtext   NOT NULL,
-    `isbn`             varchar(15)  NOT NULL,
-    `image`            blob         NOT NULL,
-    `price`            float        NOT NULL,
-    `publish_date`     date         NOT NULL,
-    `last_update_time` datetime     NOT NULL,
-    `category_id`      int(11)      NOT NULL,
-    PRIMARY KEY (`book_id`),
-    UNIQUE KEY `book_id_UNIQUE` (`book_id`),
-    UNIQUE KEY `title_UNIQUE` (`title`),
-    KEY `category_fk_idx` (`category_id`),
-    CONSTRAINT `category_fk` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 32
-  DEFAULT CHARSET = utf8;
-
-
-CREATE TABLE `customer`
-(
-    `customer_id`   int(11)      NOT NULL AUTO_INCREMENT,
-    `email`         varchar(64)  NOT NULL,
-    `fullname`      varchar(30)  NOT NULL,
-    `address`       varchar(128) NOT NULL,
-    `city`          varchar(32)  NOT NULL,
-    `country`       varchar(64)  NOT NULL,
-    `phone`         varchar(15)  NOT NULL,
-    `zipcode`       varchar(24)  NOT NULL,
-    `password`      varchar(16)  NOT NULL,
-    `register_date` datetime     NOT NULL,
-    PRIMARY KEY (`customer_id`),
-    UNIQUE KEY `customer_id_UNIQUE` (`customer_id`),
-    UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 11
-  DEFAULT CHARSET = utf8;
-
-
-CREATE TABLE `review`
-(
-    `review_id`   int(11)      NOT NULL AUTO_INCREMENT,
-    `book_id`     int(11)      NOT NULL,
-    `customer_id` int(11)      NOT NULL,
-    `rating`      int(11)      NOT NULL,
-    `headline`    varchar(128) NOT NULL,
-    `comment`     varchar(500) NOT NULL,
-    `review_time` datetime     NOT NULL,
-    PRIMARY KEY (`review_id`),
-    UNIQUE KEY `review_id_UNIQUE` (`review_id`),
-    KEY `book_fk_idx` (`book_id`),
-    KEY `customer_fk_idx` (`customer_id`),
-    CONSTRAINT `book_fk` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT `customer_fk` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 15
-  DEFAULT CHARSET = utf8;
-
-CREATE TABLE `book_order`
-(
-    `order_id`         int(11)      NOT NULL AUTO_INCREMENT,
-    `customer_id`      int(11)      NOT NULL,
-    `order_date`       datetime     NOT NULL,
-    `shipping_address` varchar(256) NOT NULL,
-    `recipient_name`   varchar(30)  NOT NULL,
-    `recipient_phone`  varchar(15)  NOT NULL,
-    `payment_method`   varchar(20)  NOT NULL,
-    `total`            float        NOT NULL,
-    `status`           varchar(20)  NOT NULL,
-    PRIMARY KEY (`order_id`),
-    UNIQUE KEY `order_id_UNIQUE` (`order_id`),
-    KEY `customer_fk_2_idx` (`customer_id`),
-    CONSTRAINT `customer_fk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 22
-  DEFAULT CHARSET = utf8;
-
-CREATE TABLE `order_detail`
-(
-    `order_id` int(11) DEFAULT NULL,
-    `book_id`  int(11) DEFAULT NULL,
-    `quantity` int(11) NOT NULL,
-    `subtotal` float   NOT NULL,
-    KEY `order_fk_idx` (`order_id`),
-    KEY `book_fk_2_idx` (`book_id`),
-    CONSTRAINT `book_fk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT `order_fk` FOREIGN KEY (`order_id`) REFERENCES `book_order` (`order_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
