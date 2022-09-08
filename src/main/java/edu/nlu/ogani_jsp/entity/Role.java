01f959ca2bf6d371f0b33291e4000775fd9bd147
thonghp <thonghoangpham123@gmail.com>
2022-09-08 16:20:54 +0700

    delete persistence in role and user entity

--- src/main/java/edu/nlu/ogani_jsp/entity/Role.java
+++ src/main/java/edu/nlu/ogani_jsp/entity/Role.java
@@ -1,35 +1,9 @@
 package edu.nlu.ogani_jsp.entity;
 
-import jakarta.persistence.*;
+import lombok.Data;
 
-@Entity
-@Table(name = "role")
+@Data
 public class Role {
     private Integer roleId;
     private String name;
-
-    @Id
-    @Column(name = "role_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Integer getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Integer roleId) {
-        this.roleId = roleId;
-    }
-
-    @Column(length = 40, nullable = false, unique = true)
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Role{" + "roleId=" + roleId + ", name='" + name + '\'' + '}';
-    }
 }
--- src/main/java/edu/nlu/ogani_jsp/entity/User.java
+++ src/main/java/edu/nlu/ogani_jsp/entity/User.java
@@ -1,11 +1,9 @@
 package edu.nlu.ogani_jsp.entity;
 
-import jakarta.persistence.*;
+import lombok.Data;
 
-@Entity
-@Table(name = "user")
+@Data
 public class User {
-
     private Integer userId;
     private String email;
     private String password;
@@ -13,69 +11,4 @@ public class User {
     private boolean enabled;
     private String photos;
     private Role role;
-
-    @Id
-    @Column(name = "user_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    @Column(length = 128, unique = true, nullable = false)
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Column(length = 64, nullable = false)
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Column(name = "full_name", length = 150, nullable = false)
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    @Column(length = 64)
-    public String getPhotos() {
-        return photos;
-    }
-
-    public void setPhotos(String photos) {
-        this.photos = photos;
-    }
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "role_id", nullable = false)
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
