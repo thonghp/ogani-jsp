749fbea49e262d5c7a0b3087c4b0f9f358a13f4f
thonghp <thonghoangpham123@gmail.com>
2022-09-11 15:45:58 +0700

    add test for database connection function

---
+++ src/test/java/edu/nlu/ogani_jsp/util/DBUtilTest.java
@@ -0,0 +1,19 @@
+package edu.nlu.ogani_jsp.util;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+public class DBUtilTest {
+
+    @Test
+    public void testConnection() throws SQLException {
+        Connection conn = DBUtil.makeConnection();
+        DatabaseMetaData dm = conn.getMetaData();
+        System.out.println("Driver name: " + dm.getDriverName());
+        System.out.println("Driver version: " + dm.getDriverVersion());
+        conn.close();
+    }
+}
