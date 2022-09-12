ba4a2e549c21474b1c8df3238bd457b7175865ce
thonghp <thonghoangpham123@gmail.com>
2022-09-12 18:30:52 +0700

    update role and user entity

--- src/main/java/edu/nlu/ogani_jsp/entity/Role.java
+++ src/main/java/edu/nlu/ogani_jsp/entity/Role.java
@@ -1,8 +1,12 @@
 package edu.nlu.ogani_jsp.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
     private Integer roleId;
     private String name;
