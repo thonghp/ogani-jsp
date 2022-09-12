aa8e76919c8a94a6b9d9201a12b25345e63b1e33
thonghp <thonghoangpham123@gmail.com>
2022-09-12 18:31:43 +0700

    update repository interface

--- src/main/java/edu/nlu/ogani_jsp/repository/Repository.java
+++ src/main/java/edu/nlu/ogani_jsp/repository/Repository.java
@@ -1,14 +1,14 @@
 package edu.nlu.ogani_jsp.repository;
 
-public interface Repository<T> {
+public interface Repository<T, ID> {
 
     T save(T t);
 
     T update(T t);
 
-    T findById(Object id);
+    T findById(ID id);
 
-    void delete(Object id);
+    void delete(ID id);
 
     Iterable<T> findAll();
 
