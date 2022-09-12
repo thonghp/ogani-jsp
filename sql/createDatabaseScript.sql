348722bd4fbccb018908126110467496c51a995c
thonghp <thonghoangpham123@gmail.com>
2022-09-12 18:35:38 +0700

    update sql script file

--- sql/createDatabaseScript.sql
+++ sql/createDatabaseScript.sql
@@ -32,6 +32,7 @@ CREATE TABLE `user`
   AUTO_INCREMENT = 3
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
+  
 
 CREATE TABLE `category`
 (
@@ -45,3 +46,4 @@ CREATE TABLE `category`
   AUTO_INCREMENT = 11
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
+  
