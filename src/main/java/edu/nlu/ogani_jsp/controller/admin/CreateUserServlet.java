22a750117dba3cb33b3de26a02b36812a35598ab
thonghp <thonghoangpham123@gmail.com>
2022-09-15 17:14:43 +0700

    add controller to handle saving user

---
+++ src/main/java/edu/nlu/ogani_jsp/controller/admin/CreateUserServlet.java
@@ -0,0 +1,28 @@
+package edu.nlu.ogani_jsp.controller.admin;
+
+import edu.nlu.ogani_jsp.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CreateUserServlet", urlPatterns = "/admin/create_user")
+public class CreateUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserService service = new UserService();
+
+        service.save(request);
+
+        service.listUser(request, response);
+    }
+}
