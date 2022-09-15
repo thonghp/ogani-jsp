e17080c33d4b97cea4ea4163207d1f4e25749b8a
thonghp <thonghoangpham123@gmail.com>
2022-09-15 16:37:27 +0700

    refactor the listUser function for reuse

--- src/main/java/edu/nlu/ogani_jsp/controller/admin/ListUserServlet.java
+++ src/main/java/edu/nlu/ogani_jsp/controller/admin/ListUserServlet.java
@@ -1,16 +1,13 @@
 package edu.nlu.ogani_jsp.controller.admin;
 
-import edu.nlu.ogani_jsp.entity.User;
 import edu.nlu.ogani_jsp.service.UserService;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(name = "ListUserServlet", urlPatterns = "/admin/list_users")
 public class ListUserServlet extends HttpServlet {
@@ -19,13 +16,7 @@ public class ListUserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         UserService service = new UserService();
 
-        List<User> users = service.listUser();
-        request.setAttribute("listUsers", users);
-
-        String listPage = "users.jsp";
-
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher(listPage);
-        requestDispatcher.forward(request, response);
+        service.listUser(request, response);
     }
 
     @Override
