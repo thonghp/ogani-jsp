ea6ab83802d5f6ca41827af19df1010f2522ad7d
thonghp <thonghoangpham123@gmail.com>
2022-09-18 23:05:55 +0700

    chore: add data for sql table

---
+++ sql/data.sql
@@ -0,0 +1,6 @@
+-- Role
+INSERT INTO `role` VALUES (1,'admin'),(2,'employee');
+
+-- User
+INSERT INTO `user` VALUES (1,'admin1@gmail.com','admin123','admin1',_binary '',NULL,1),
+                          (2,'admin2@gmail.com','admin123','admin2',_binary '',NULL,1);
